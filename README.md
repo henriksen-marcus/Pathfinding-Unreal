8daa5f2b5bb8e407dd5dcf6f0ec85a6f11385bbc
Marcus Henriksen <89453098+henriksen-marcus@users.noreply.github.com>
2022-10-12 17:09:23 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Pathfinding
+
+In this project we develop Djikstra's algorithm and A* algorithm in unreal C++.
+
+Developed with Unreal Engine 5
---
+++ Source/Pathfinding/PathfindingGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PathfindingGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PATHFINDING_API APathfindingGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
